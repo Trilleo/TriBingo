a9484840fdc28d8094fa244113c52cadb14407c4
Trilleo <gongsanle@gmail.com>
2026-05-22 21:30:49 +0800

    Feature: Add GameListener for essential game logic

---
+++ src/main/kotlin/net/trilleo/mc/plugins/tribingo/listeners/GameListener.kt
@@ -0,0 +1,17 @@
+package net.trilleo.mc.plugins.tribingo.listeners
+
+import net.trilleo.mc.plugins.tribingo.managers.ItemManager
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class GameListener(private val plugin: JavaPlugin) : Listener {
+    // Give Main Item on join
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        ItemManager(plugin).clearPluginItems(player)
+        ItemManager(plugin).updatePluginItem(player)
+    }
+}
