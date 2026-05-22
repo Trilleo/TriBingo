4fd14cde2d4b6758f305d593c09fe27ce7159f49
Trilleo <gongsanle@gmail.com>
2026-05-22 21:16:27 +0800

    Feature: Add plugin item update logic to ItemManager

--- src/main/kotlin/net/trilleo/mc/plugins/tribingo/managers/ItemManager.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/tribingo/managers/ItemManager.kt
@@ -1,5 +1,7 @@
 package net.trilleo.mc.plugins.tribingo.managers
 
+import net.trilleo.mc.plugins.tribingo.data.ServerDataManager
+import net.trilleo.mc.plugins.tribingo.items.MainItem
 import net.trilleo.mc.plugins.tribingo.utils.PDCEntryUtil
 import net.trilleo.mc.plugins.tribingo.utils.PDCUtil
 import org.bukkit.entity.Player
@@ -18,6 +20,22 @@ class ItemManager(private val plugin: JavaPlugin) {
                 )
             ) {
                 player.inventory.remove(item)
+            }
+        }
+    }
+
+    fun updatePluginItem(player: Player) {
+        val serverData = ServerDataManager.get()
+
+        ItemManager(plugin).clearPluginItems(player)
+
+        if (serverData.getString("bingo_game_state") == "INACTIVE") {
+            val mainItem = MainItem(plugin).create()
+
+            if (player.inventory.getItem(8) == null) {
+                player.inventory.setItem(8, mainItem)
+            } else {
+                player.inventory.addItem(mainItem)
             }
         }
     }
