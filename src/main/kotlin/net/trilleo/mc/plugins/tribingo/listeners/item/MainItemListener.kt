04913450c40ba1c0f7a9f66a993a12da58dfa933
Trilleo <gongsanle@gmail.com>
2026-05-23 09:29:25 +0800

    Feature: Add listener for the Main Item

--- src/main/kotlin/net/trilleo/mc/plugins/tribingo/listeners/item/MainItemListener.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/tribingo/listeners/item/MainItemListener.kt
@@ -1,4 +1,54 @@
 package net.trilleo.mc.plugins.tribingo.listeners.item
 
-class MainItemListener {
+import net.trilleo.mc.plugins.tribingo.data.ServerDataManager
+import net.trilleo.mc.plugins.tribingo.registration.GUIManager
+import net.trilleo.mc.plugins.tribingo.utils.PDCEntryUtil
+import net.trilleo.mc.plugins.tribingo.utils.PDCUtil
+import net.trilleo.mc.plugins.tribingo.utils.sendPrefixed
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.plugin.java.JavaPlugin
+
+class MainItemListener(private val plugin: JavaPlugin) : Listener {
+    // Detect menu opening
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val item = event.item
+        if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK) {
+            if (item != null && PDCUtil.get(
+                    item,
+                    PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                    PersistentDataType.STRING
+                ) == PDCEntryUtil.PDCValue().mainItemIdentifier
+            ) {
+                event.isCancelled = true
+                val serverData = ServerDataManager.get()
+
+                if (serverData.getString("bingo_game_state", "INACTIVE") == "INACTIVE") {
+                    GUIManager.open(player, "main")
+                } else {
+                    GUIManager.open(player, "bingo_board")
+                }
+            }
+        }
+    }
+
+    // Detect main item dropping
+    @EventHandler
+    fun onPlayerDropItem(event: PlayerDropItemEvent) {
+        val item = event.itemDrop.itemStack
+        if (PDCUtil.get(
+                item,
+                PDCEntryUtil.PDCKey(plugin).itemIdentifierKey,
+                PersistentDataType.STRING
+            ) == PDCEntryUtil.PDCValue().mainItemIdentifier
+        ) {
+            event.isCancelled = true
+        }
+    }
 }
